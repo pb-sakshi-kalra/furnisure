7bbb90b31dc596235ca1d2a36b800559545bb8e4
sk24tw <sakshikalra@thoughtwin.com>
2024-11-11 13:10:39 -0600

    gtag fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import "./index.css";
 import Home from "./pages/Home";
 import Event from "./pages/Event";
@@ -10,14 +10,24 @@ import ScrollToTop from "./pages/ScrollToTop";
 import HomeEvent from "./pages/HomeEvent";
 import AboutUs from "./components/AboutUs";
 import ContactUs from "./components/ContactUs";
-import PageViewTracker from "./PageViewTracker";
 
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    const fullUrl = window.location.href;
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+      event: "pageview",
+      page_path: fullUrl,
+    });
+    document.title = `Page - ${fullUrl}`;
+  }, [location]);
+
   return (
     <div>
       <ScrollToTop />
       <Routes>
-        <PageViewTracker />
         <Route path="/" element={<Home />} />
         <Route path="/event" element={<Event />} />
         <Route path="/home" element={<HomeEvent />} />
